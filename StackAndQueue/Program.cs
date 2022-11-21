6eed85d87fbb9d05db2d6dd79e9e6d9b6a5f92f9
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-21 13:53:38 +0530

    [Lalit] Add: Creating a Stack

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace StackAndQueue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue problem Statement");
+            bool condition = true;
+            while (condition)
+            {
+                Console.WriteLine("1. Pushing the Node to Stack\n2. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                Stacks stacks = new Stacks();
+                switch (choice)
+                {
+                    case 1:
+                        stacks.Push(56);
+                        stacks.Push(30);
+                        stacks.Push(70);
+                        stacks.Display();
+                        break;
+                    default:
+                        condition = false;
+                        break;
+                }
+            }
+        }
+    }
+}
